8862d53a9e9c8e502f90ba009809f535766dfb5a
y4m7sdc36171vjm20wev <62202697+y4m7sdc36171vjm20wev@users.noreply.github.com>
2020-03-15 06:59:54 -0400

    Update Edcite.js
    Added automated answer select

--- Edcite.js
+++ Edcite.js
@@ -20,9 +20,19 @@ const check = () => {
     for(let i = 0; i < userData.result.qdatas[itemnumber].answer.answers[0].choices.length; i++){
         if(userData.result.qdatas[item].answer.answers[0].choices[i].correct == true){
             let quest = i+1;
-            document.getElementsByClassName('extitle')[0].innerHTML = "Question: " + itemnumber + "// Answer: " + quest;
+            let url = window.location.href.split("#")[1]
+            //if you add #show to the url it will show the answers
+            //I added that option just in case it doesn't automatically click the answers
+            if(url == "show"){
+                document.getElementsByClassName('extitle')[0].innerHTML = "Question: " + itemnumber + "// Answer: (" + quest + ") " + userData.result.qdatas[item].answer.answers[0].choices[i].html;
+            }
+            for(let k = 0; k < document.getElementsByClassName('cmcv_text').length; k++){
+                if(userData.result.qdatas[item].answer.answers[0].choices[i].html == document.getElementsByClassName('cmcv_text')[k].innerHTML){
+                    document.getElementsByClassName('cmcv_text')[k].click();
+                }
+            }
             
         }
     }
 }
-setInterval(function(){ check() }, 300);
+setInterval(function(){ check() }, 600);
